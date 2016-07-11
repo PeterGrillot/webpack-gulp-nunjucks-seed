5615a84a640f2ebb299abffee1992c03e2261d34
Peter Grillot <petergrillot@gmail.com>
2016-07-11 15:18:21 -0400

    Added a basic block structure as well as production env variables
    Took out fonts and rev as well

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,14 +7,54 @@ webpackJsonp([0,1],[
 
 /***/ },
 /* 1 */
-/***/ function(module, exports) {
-
-	"use strict";
+/***/ function(module, exports, __webpack_require__) {
 
 	// *********************
 	//    Modules scripts
 	// *********************
-	// import 'modules/module.js';
+	
+	// MINIFIED Vendor file should be copied over via copyScripts.js (it is by default)
+	
+	// USING production variables is simple
+	// Burn after reading
+	'use strict';
+	
+	// IMPORT all modules here. Keep lib and minified files out this file.
+	
+	__webpack_require__(2);
+	
+	if (true) {
+		var dev_var = 'url/to/dev';
+	}
+	if (false) {
+		var dev_var = 'url/to/prod';
+	}
+	console.log(dev_var);
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	// IMPORTS
+	
+	// FUNCTION
+	'use strict';
+	
+	(function () {
+		'use strict';
+		window.onload = function () {
+			var bLazy = new Blazy({
+				breakpoints: [{ width: 640, src: 'data-src-sm' }],
+				success: function success(element) {
+					setTimeout(function () {
+						var parent = element.parentNode;
+						parent.className = parent.className.replace(/\bloading\b/, '');
+					}, 2000);
+				}
+			});
+			setTimeout(bLazy.revalidate(), 200);
+		};
+	})();
 
 /***/ }
 ]);
