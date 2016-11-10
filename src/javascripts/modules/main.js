ad5ffa90852ef5ad83dfbc8d71ca5ccac9ad5abc
Peter Grillot <petergrillot@gmail.com>
2016-11-09 23:14:14 -0500

    Updates Done

--- src/javascripts/modules/main.js
+++ src/javascripts/modules/main.js
@@ -8,37 +8,35 @@ import Parallax from 'lib/parallax';
 	// USAGE
 	const prop = 'top';
 	const factor = 3;
-	window.addEventListener('scroll', ()=>{
+	window.addEventListener('scroll', () => {
 		new Parallax(prop, factor);
 	});
 
 	// Request
-var request = new XMLHttpRequest();
-var data;
-request.open('GET', 'https://spreadsheets.google.com/feeds/list/1pstEHIoEiQiNtYlTTEIygRJaOVVRVUhAy6BGVzNGm20/od6/public/values?alt=json', true);
+	var request = new XMLHttpRequest();
+	var data;
+	request.open('GET', 'https://spreadsheets.google.com/feeds/list/1pstEHIoEiQiNtYlTTEIygRJaOVVRVUhAy6BGVzNGm20/od6/public/values?alt=json', true);
 
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    // Success!
-    var data = JSON.parse(request.responseText);
-    for (var i = 0; i < data.feed.entry.length; i++) {
-        var list = document.getElementById('google_doc_list');
-        var entry = data.feed.entry[i];
-        list.innerHTML = list.innerHTML + 
-        '<div class="list"><p><a href="' + entry.gsx$url.$t+'">'+entry.gsx$roster.$t +'</a></p><p>'+entry.gsx$date.$t+' - '+entry.gsx$venue.$t+' - '+entry.gsx$location.$t+'</p></div>'
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			// Success!
+			var data = JSON.parse(request.responseText);
+			for (var i = 0; i < data.feed.entry.length; i++) {
+				var list = document.getElementById('google_doc_list');
+				var entry = data.feed.entry[i];
+				list.innerHTML = list.innerHTML +
+					'<div class="list"><p><a href="' + entry.gsx$url.$t + '">' + entry.gsx$roster.$t + '</a></p><p>' + entry.gsx$date.$t + ' - ' + entry.gsx$venue.$t + ' - ' + entry.gsx$location.$t + '</p></div>'
 
-        // data.feed.entry[i].gsx$date
-    }
-  } else {
-    // We reached our target server, but it returned an error
-    console.log(`There was an error: ${request.status}`);
-  }
-};
+				// data.feed.entry[i].gsx$date
+			}
+		} else {
+			// We reached our target server, but it returned an error
+			console.log(`There was an error: ${request.status}`);
+		}
+	};
 
-request.onerror = function() {
-  	console.log('There was a connection error of some sort');
-};
-
-console.log(request.send());
-
+	request.onerror = function() {
+		console.log('There was a connection error of some sort');
+	};
+	request.send();
 })();
